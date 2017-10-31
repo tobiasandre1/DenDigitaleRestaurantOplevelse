8cf6b702c5e43a9066c749d36ac53f44fd769f2b
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-11-01 00:50:17 +0100

    Custom toolbar setup

--- app/src/main/java/gruppe24/dendigitalerestaurantoplevelse/MainActivity.java
+++ app/src/main/java/gruppe24/dendigitalerestaurantoplevelse/MainActivity.java
@@ -2,13 +2,16 @@ package gruppe24.dendigitalerestaurantoplevelse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Ændrer det her noget?
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
