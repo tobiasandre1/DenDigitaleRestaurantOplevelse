23fb53d5f4d9d7009bd81a7bf90665f096265e39
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-11-01 16:28:59 +0100

    Changed implementation of custom toolbar
    Classes using the custom toolbar should extend the class CustomToolbarActivity and include the toolbar in their xml

--- app/src/main/java/gruppe24/dendigitalerestaurantoplevelse/CustomToolbarActivity.java
+++ app/src/main/java/gruppe24/dendigitalerestaurantoplevelse/CustomToolbarActivity.java
@@ -1,16 +1,25 @@
+/**
+ * Created by Tobias on 01-11-2017.
+ */
+
 package gruppe24.dendigitalerestaurantoplevelse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class MainActivity extends CustomToolbarActivity{
+public class CustomToolbarActivity extends AppCompatActivity{
 
+    protected Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
 
     }
+
 }
