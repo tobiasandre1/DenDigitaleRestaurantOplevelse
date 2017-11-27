d9b8671fba2b3cc90851825fb81e02d8017b56cd
tobiasandre1 <hoejsgaard.tobias@gmail.com>
2017-11-27 15:29:46 +0100

    Added favorites to user class

--- app/src/main/java/gruppe24/dendigitalerestaurantoplevelse/backend/User.java
+++ app/src/main/java/gruppe24/dendigitalerestaurantoplevelse/backend/User.java
@@ -1,5 +1,8 @@
 package gruppe24.dendigitalerestaurantoplevelse.backend;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Tobias on 27-11-2017.
  */
@@ -7,8 +10,10 @@ package gruppe24.dendigitalerestaurantoplevelse.backend;
 public class User {
 
     Order shoppingCart;
+    List<CharSequence> favorites;
 
     public User(){
         shoppingCart = new OrderArrayList();
+        favorites = new ArrayList<>();
     }
 }
