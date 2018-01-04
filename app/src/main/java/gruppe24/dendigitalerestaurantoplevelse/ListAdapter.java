cc1658a4f384fdf831f9dd6327a6b4e6fbe63ef5
mads118 <vpmads@gmail.com>
2018-01-04 14:08:12 +0100

    work in progress on buttonclicklistener

--- app/src/main/java/gruppe24/dendigitalerestaurantoplevelse/ListAdapter.java
+++ app/src/main/java/gruppe24/dendigitalerestaurantoplevelse/ListAdapter.java
@@ -1,17 +1,26 @@
 package gruppe24.dendigitalerestaurantoplevelse;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.LayoutInflater;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
 
 import gruppe24.dendigitalerestaurantoplevelse.backend.Backend;
 import gruppe24.dendigitalerestaurantoplevelse.backend.Dish;
+import gruppe24.dendigitalerestaurantoplevelse.fragments.FragFoodInfo;
 
 public class ListAdapter extends ArrayAdapter<String> {
 
@@ -34,6 +43,25 @@ public class ListAdapter extends ArrayAdapter<String> {
             TextView itemText = (TextView) customView.findViewById(R.id.nameID);
             ImageView itemImage = (ImageView) customView.findViewById(R.id.itemImageID);
             TextView itemPriceText = (TextView) customView.findViewById(R.id.priceID);
+            itemImage.setOnClickListener(new OnClickListener() {
+                public void onClick(View v) {
+                    Fragment fragment = new FragFoodInfo();
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.frag_place, fragment)
+                            .commit();
+
+
+
+
+                    /* AlertDialog.Builder builder1 = new AlertDialog.Builder(getContext());
+                    builder1.setMessage("Her vises foodinfo");
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                    */
+                }
+            });
+
 
 
             itemText.setText(dish.getName());
@@ -43,6 +71,9 @@ public class ListAdapter extends ArrayAdapter<String> {
         } catch(NullPointerException e){
             System.out.println("Could not find dish named: " + dishName);
         }
+
+
+
 
         return customView;
     }
