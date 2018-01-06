914d6a5fbbd5972bd42a4812ab3534eaf3af8517
Tobias Højsgaard <hoejsgaard.tobias@gmail.com>
2018-01-07 00:22:00 +0100

    Fragmenu was using the wrong layout

--- app/src/main/java/gruppe24/dendigitalerestaurantoplevelse/fragments/FragMenu.java
+++ app/src/main/java/gruppe24/dendigitalerestaurantoplevelse/fragments/FragMenu.java
@@ -21,6 +21,6 @@ public class FragMenu extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragsearch, container, false);
+        return inflater.inflate(R.layout.fragment_fragmenu, container, false);
     }
 }
